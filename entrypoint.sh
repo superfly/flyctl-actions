466ac9ed14e12d90b5663bef10398435c4261bef
Kurt <mrkurt@gmail.com>
2020-01-14 19:38:40 +0000

    Add FLY_PROJECT_PATH env var for working in subdirectories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,17 @@
 #!/bin/sh -l
 
+if [ -n "$FLY_PROJECT_PATH" ]; then
+  PREV_PATH=$(pwd)
+  # Allow user to change directories in which to run Fly commands
+  cd "$FLY_PROJECT_PATH" || exit
+fi
 sh -c "flyctl $*"
+
+ACTUAL_EXIT="$?"
+
+if [ -n "$PREV_PATH" ]; then
+  # If we changed directories before, we should go back to where we were.
+  cd "$PREV_PATH" || exit
+fi
+
+exit $ACTUAL_EXIT
